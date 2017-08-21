820749535d36c9cc77aa44f0888c0c00ebd59730
Rohit Singh Rajawat <rajawat.rohit8@gmail.com>
2017-08-21 15:45:50 +0530

    Update chatapp.js

--- chatapp.js
+++ chatapp.js
@@ -2,7 +2,7 @@ var app = require("http").createServer(handler),
     io = require("socket.io").listen(app),
     fs = require("fs");
 
-app.listen(1337);
+app.listen(1234);
 
 function handler(req, res) {
     fs.readFile(__dirname + '/chatappweb.html',
